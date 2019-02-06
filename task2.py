3b4af8dfbe8d6e929e248b7aec20f3581723cd3b
sujanrp <47112995+sujanrp@users.noreply.github.com>
2019-02-06 10:28:57 -0800

    Delete task2.py

--- task2.py
+++
@@ -1,6 +0,0 @@
-l = []
-with open('running-config.cfg') as f:
- newText=f.read().replace('192', '10')
- newText=f.read().replace('172', '10')
-with open('running-config.cfg', "w") as f:
- f.write(newText)
